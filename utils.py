ea199bc32a9fb4b9526e5ba7605be3f33495be0b
chaitanya Vardhan <cvkandula1729@gmail.com>
2024-08-25 16:37:54 -0500

    pushing cypher generator changes to prod

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from neo4j import GraphDatabase, TRUST_ALL_CERTIFICATES
 import streamlit as st
 from langchain_community.graphs import Neo4jGraph
 
@@ -6,7 +7,8 @@ graph = Neo4jGraph(
     url=st.secrets["NEO4J_URI"],
     username=st.secrets["NEO4J_USERNAME"],
     password=st.secrets["NEO4J_PASSWORD"],
-    database="supplychaindb"
+    database="neo4j",
+    driver_config={"encrypted": True, "trust": TRUST_ALL_CERTIFICATES}
 )
 
 print(graph.query("Match (n) return n limit 10"))
