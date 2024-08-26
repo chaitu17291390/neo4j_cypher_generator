d770eb1786998f33d89548950fb94e369e9aafe6
chaitanya Vardhan <cvkandula1729@gmail.com>
2024-08-25 22:48:01 -0500

    updated github hide

--- bot.py
+++ bot.py
@@ -1,21 +1,13 @@
 import streamlit as st
 from final_agent import run_agent
 
-# Page Config (This should be the first Streamlit command)
-st.set_page_config("Ebert", page_icon=":movie_camera:")
+# tag::setup[]
+# Page Config
 
-# Hide GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
-def write_message(role, content, save=True):
+def write_message(role, content, save = True):
     """
     This is a helper function that saves a message to the
-    session state and then writes a message to the UI
+     session state and then writes a message to the UI
     """
     # Append to session state
     if save:
@@ -25,24 +17,31 @@ def write_message(role, content, save=True):
     with st.chat_message(role):
         st.markdown(content)
 
+st.set_page_config("Ebert", page_icon=":movie_camera:")
+# end::setup[]
+
+# tag::session[]
 # Set up Session State
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "assistant", "content": "Hi, I'm the SCM Chatbot! How can I help you?"},
+        {"role": "assistant", "content": "Hi, I'm the SCM Chatbot!  How can I help you?"},
     ]
+# end::session[]
 
+# tag::submit[]
 # Submit handler
 def handle_submit(message):
     # Handle the response
     with st.spinner('Thinking...'):
         response = run_agent(message)
         write_message('assistant', response)
+# end::submit[]
 
+
+# tag::chat[]
 # Display messages in Session State
 for message in st.session_state.messages:
-    # Ensure that each message is displayed in its own context
-    with st.chat_message(message['role']):
-        st.markdown(message['content'])
+    write_message(message['role'], message['content'], save=False)
 
 # Handle any user input
 if prompt := st.chat_input("What is up?"):
@@ -51,3 +50,4 @@ if prompt := st.chat_input("What is up?"):
 
     # Generate a response
     handle_submit(prompt)
+# end::chat[]
